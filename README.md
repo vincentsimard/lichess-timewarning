736184beb96e50a0069ce1c4897793a4f26681dc
Vincent Simard <vincent@vincentsimard.com>
2014-10-18 00:40:13 -0400

    First version

---
+++ README.md
@@ -0,0 +1,4 @@
+lichess-timewarning
+===================
+
+lichess-timewarning is a Google Chrome extension that warns players that their time is running out.
---
+++ app/scripts/lichess-timewarning.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var lichess = {};
+
+lichess.el = document.querySelector('#lichess');
+lichess.elTable = lichess.el.querySelector('.lichess_table');
+lichess.elBoard = lichess.el.querySelector('.lichess_board');
+lichess.elPlayerClock = lichess.el.querySelector('.clock_bottom');
+
+lichess.isGameOver = function() {
+  return this.elTable.classList.contains('finished');
+};
+
+var makeAudio = function(file, volume) {
+  var audio = new Audio(chrome.extension.getURL('ogg/' + file));
+  audio.volume = volume;
+
+  return audio;
+};
+
+var timeWarning = function() {
+  var audio = makeAudio('Lowtime.wav', 1);
+  audio.play();
+};
+
+var handleMutation = function(mutations) {
+  var firstTarget = mutations[0].target;
+  var classes = firstTarget.classList;
+
+  var hasEmerg = classes.contains('emerg');
+  var hasOutOfTime = classes.contains('outoftime');
+  
+  if (hasEmerg && !hasOutOfTime && !lichess.isGameOver()) {
+    timeWarning();
+  }
+};
+
+var init = function() {
+  if (!lichess.elPlayerClock) { return; }
+
+  var emergObserver = new MutationObserver(handleMutation);
+  var config = { attributeFilter: ['class'] };
+  
+  emergObserver.observe(lichess.elPlayerClock, config);
+};
+
+init();
